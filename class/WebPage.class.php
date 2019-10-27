235def8a75c3c917f4fb1efaaf40b4a451d1a04a
Mehdi KHENISSI <mehdi.khenissi@etudiant.univ-reims.fr>
2019-10-27 20:45:47 +0100

    Update WebPage.class.php

--- class/WebPage.class.php
+++ class/WebPage.class.php
@@ -60,6 +60,7 @@ class WebPage
             
             <head>{$this->head}
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+             <meta  name = "robots" content="noindex , nofollow">
             <title>{$this->title}</title>
             </head>
             <body>
